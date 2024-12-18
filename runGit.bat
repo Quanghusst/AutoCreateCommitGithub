5b712ce269a75271b25a55c1e3446cd75892ba16
Lê Đăng Quang <116963134+Quanghusst@users.noreply.github.com>
2024-12-18 23:22:54 +0700

    Update runGit.bat

--- runGit.bat
+++ runGit.bat
@@ -1,29 +1,16 @@
 @echo off
+:: Lấy tên người dùng (chủ sở hữu) repository bằng gh
+for /f "delims=" %%u in ('gh repo view --json owner --jq ".owner.login"') do set ownerName=%%u
+echo Repository Owner: %ownerName%
+
+for /f "delims=" %%u in ('gh api user --jq ".login"') do set username=%%u
+echo GitHub Username: %username%
 for %%F in (%cd%) do set folderName=%%~nxF
 echo %folderName%
-:: Lấy giờ và phút từ %time%
-for /f "tokens=1-4 delims=:. " %%a in ("%time%") do (
-    set hh=%%a
-    set mm=%%b
-)
 
-:: Xử lý định dạng 12 giờ và AM/PM
-set ap=AM
-if %hh% geq 12 (
-    set ap=PM
-    if %hh% gtr 12 set /a hh=hh-12
-)
-if %hh%==0 set hh=12
 
-:: Lấy ngày từ %date% và loại bỏ ngày trong tuần nếu có
-for /f "tokens=2-4 delims=, " %%d in ('echo %date%') do (
-    for /f "tokens=1-3 delims=/-. " %%x in ("%%d") do (
-        set mm=%%x
-        set dd=%%y
-        set yyyy=%%z
-    )
-)
-
+SET /P branchString=Please enter a branch: 
+SET /P messageString=Please enter a message: 
 echo ========================== CREATE REPO ======================================
 echo.   
 gh repo create %folderName% --public --confirm
@@ -40,7 +27,7 @@ git pull origin main
 git add .
 git commit -m "Commit at %hh%:%mm% %ap% %dd%/%mm%/%yyyy%"
 git branch -M main
-git remote add origin https://github.com/Quanghusst/%folderName%.git
+git remote add origin https://github.com/%ownerName%/%folderName%.git
 git push -u origin main
 echo.
 echo ================================ END ========================================
